b3e1dead9307c44a75f117a2e70ca1e19cd331c0
spider-cashew-chia <65230310+spider-cashew-chia@users.noreply.github.com>
2025-04-10 13:31:21 +0100

    Update page.php

--- theme-ebp/page.php
+++ theme-ebp/page.php
@@ -21,12 +21,7 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			the_content();
 
 		endwhile; // End of the loop.
 		?>
@@ -34,5 +29,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
